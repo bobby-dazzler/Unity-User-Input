5981d20a4a9bfdda8b27a584cbe501b0f9f15b3b
bobby-dazzler <philipanderson@outlook.com>
2019-04-16 16:02:38 +0100

    Initial Commit

---
+++ Base/UserInputMono.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputMono : MonoBehaviour {
+
+    public ShipForcesData shipForcesData;
+
+    void Awake() {
+
+    }
+
+    void FixedUpdate() {
+        if (Input.GetKey("w")) {
+            if (shipForcesData.currentJetPower < shipForcesData.maxPower) {
+                shipForcesData.currentJetPower++;
+            } 
+        } else if (Input.GetKey("s")) {
+            if (shipForcesData.currentJetPower > -shipForcesData.maxPower) {
+                shipForcesData.currentJetPower--;
+            } 
+        } else {
+            if (shipForcesData.currentJetPower > 0) {
+                shipForcesData.currentJetPower--;
+            } else if (shipForcesData.currentJetPower < 0) {
+                shipForcesData.currentJetPower++;
+            }
+        }
+        
+        //shipForcesData.horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey("a")) {
+            shipForcesData.isSteeringLeft = true;
+            //shipForcesData.horizontalInput = Input.GetAxis("Horizontal");
+            shipForcesData.waterJetRotation_Y = shipForcesData.waterJetTransform.localEulerAngles.y + 2f;
+        } else {
+            shipForcesData.isSteeringLeft = false;
+        } 
+        
+        if (Input.GetKey("d")) {
+            shipForcesData.isSteeringRight = true;
+            //shipForcesData.horizontalInput = Input.GetAxis("Horizontal");
+            shipForcesData.waterJetRotation_Y = shipForcesData.waterJetTransform.localEulerAngles.y - 2f;
+        } else {
+            shipForcesData.isSteeringRight = false;
+        }
+
+    }
+}
